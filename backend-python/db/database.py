b3cca2390ebb3b90ab7ae85ed2e105c978378ce6
Virendra Bodele <50671596+Virendrabodele@users.noreply.github.com>
2026-03-03 20:42:28 +0530

    Update database.py

--- backend-python/db/database.py
+++ backend-python/db/database.py
@@ -2,6 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
 
+class Base(DeclarativeBase):
+    pass
+
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.ENV == "development",
